fc10240120d24b50e5162e7186769ff316c260b8
Gabriek0 <gabriel.sanches170@gmail.com>
2022-06-16 20:29:28 -0300

    feat: config colors and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+    colors: {
+        gray: {
+            "900": "#181823",
+            "800": "#1F2029",
+            "700": "#353646",
+            "600": "#4B4D63",
+            "500": "#616480",
+            "400": "#797d9A",
+            "300": "#9699B0",
+            "200": "#B3B5C6",
+            "100": "#D1D2DC",
+            "50": "#EEEEF2"
+        },
+
+    },
+    styles: {
+        global: {
+            body: {
+                //bg -> background
+                bg: "gray.900",
+                color: "gray.50"
+            }
+        }
+    }
+});
