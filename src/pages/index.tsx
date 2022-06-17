d8100260e2455a5b7efbb5ca6fb8132539ffc02f
Gabriek0 <gabriel.sanches170@gmail.com>
2022-06-16 22:43:19 -0300

    feat: add login screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,70 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      h="100vh"
+      w="100vw"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack
+          spacing="4"
+        >
+
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg" //input large
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button
+          colorScheme="pink"
+          type="submit"
+          mt="6"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
