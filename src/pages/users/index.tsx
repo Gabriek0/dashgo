8cf9cd76becd43bccd93c73807beaeb545afa514
Gabriek0 <gabriel.sanches170@gmail.com>
2022-08-13 12:12:25 -0300

    feat: add refecth loading and axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -30,6 +30,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 type User = {
   id: string;
@@ -39,11 +40,10 @@ type User = {
 };
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     ["user"],
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("/users");
 
       const users = data.users.map((user: User) => {
         return {
@@ -88,6 +88,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
